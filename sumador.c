2b76f251ede2bfe5cf8c6d50ebfd8df7540c0eb9
erickbf90 <57110993+erickbf90@users.noreply.github.com>
2020-03-12 20:41:50 -0600

    Add files via upload

---
+++ sumador.c
@@ -0,0 +1,14 @@
+
+
+#include<stdio.h>
+
+int main(){
+	int num1, num2, resultado;
+	printf("ingrese el primer número\n");
+	scanf("%d",&num1);
+	printf("ingrese el segundo número\n");
+	scanf("%d",&num2);
+	resultado = num1+num2;
+	printf("El resultado es: %d\n",resultado);
+	return 0;
+}
